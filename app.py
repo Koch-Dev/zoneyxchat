9d3caef169d6e00e6589147d477a26572e1ebac7
Chiranjib koch <93111779+Chiranjibkoch@users.noreply.github.com>
2022-12-16 22:20:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://zoneyxchat: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@github.com/Chiranjibkoch/zoneyxchat ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+k
